68360d35a3665ff9843d178d1b6cb215f2861329
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-05-19 12:07:09 +0100

    prepare for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn buckswagtail.wsgi --log-file -
--- buckswagtail/settings/production.py
+++ buckswagtail/settings/production.py
@@ -1,8 +1,28 @@
 from .base import *
 
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
+
+
 DEBUG = False
 
 try:
     from .local import *
 except ImportError:
     pass
+
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+	
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
