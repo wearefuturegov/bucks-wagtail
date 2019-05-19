803e5926913bdb45d7da9d64f2996b10bd259b35
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-05-19 19:59:13 +0100

    create api endopoint for community assets

--- buckswagtail/api.py
+++ buckswagtail/api.py
@@ -1,10 +1,18 @@
-from wagtail.api.v2.endpoints import PagesAPIEndpoint
 from wagtail.api.v2.router import WagtailAPIRouter
+
+from wagtail.api.v2.endpoints import PagesAPIEndpoint
 from wagtail.images.api.v2.endpoints import ImagesAPIEndpoint
 from wagtail.documents.api.v2.endpoints import DocumentsAPIEndpoint
+from wagtail.api.v2.endpoints import BaseAPIEndpoint
+
+from communityassets.models import CommunityAsset
+
+class CommunityAssetsAPIEndpoint(BaseAPIEndpoint):
+    model = CommunityAsset
 
 api_router = WagtailAPIRouter('wagtailapi')
 
 api_router.register_endpoint('pages', PagesAPIEndpoint)
 api_router.register_endpoint('images', ImagesAPIEndpoint)
 api_router.register_endpoint('documents', DocumentsAPIEndpoint)
+api_router.register_endpoint('community-assets', CommunityAssetsAPIEndpoint)
