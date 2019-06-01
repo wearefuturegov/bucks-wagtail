64d34025c0c7372ab738655e40110052b7d692f9
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-06-01 23:57:09 +0100

    add two popularity booleans to content pages

--- genericcontent/models.py
+++ genericcontent/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from wagtail.core.models import Page, Orderable
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, InlinePanel, FieldRowPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, InlinePanel, FieldRowPanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.core.fields import StreamField
 from wagtail.api import APIField
 from modelcluster.fields import ParentalKey
+from django.forms import CheckboxInput
 
 from streams import blocks
 
@@ -30,6 +31,8 @@ class ExternalLinks(Orderable):
 class GenericContent(Page):
     summary = models.CharField(max_length=200, blank=False, null=True, help_text="A short summary of the page that appears on the homepage and in search results")
     intro = models.CharField(max_length=400, blank=False, null=True, help_text="Appears under the title")
+    popular_advice = models.BooleanField(null=True, blank=True, help_text="Show this page as popular advice on the homepage?")
+    popular_search = models.BooleanField(null=True, blank=True, help_text="Show this page as a popular search on the homepage?")
 
     content = StreamField(
         [
@@ -45,12 +48,20 @@ class GenericContent(Page):
     api_fields = [
         APIField("summary"),
         APIField("intro"),
-        APIField("content")
+        APIField("content"),
+        APIField("popular_advice"),
+        APIField("popular_search"),
     ]
 
     content_panels = Page.content_panels + [
         FieldPanel("summary"),
         FieldPanel("intro"),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel("popular_advice", widget=CheckboxInput),
+                FieldPanel("popular_search", widget=CheckboxInput)
+            ]),
+        ], heading="Listing on homepage"),
         StreamFieldPanel("content"),
         InlinePanel("external_links", heading="External links")
     ]
